d11f817eb8ec15ec74896b3512578890d3444a18
akbcit <kumaraditya.ca@gmail.com>
2023-11-16 11:22:10 -0800

    boilerplate-commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Import express
 const express = require("express");
+// Import path
+const path = require("path");
 // Create a server
 const app = express();
 // Create a PORT
@@ -9,4 +11,12 @@ const logger = require("morgan");
 // Use morgan as logger
 app.use(logger("dev"));
 // Ask app to use public dir for static files
-app.use(express.static("/public"));
+app.use(express.static(path.join(__dirname,"/public")));
+// Other code to follow
+
+
+
+// Start the server
+app.listen(PORT,()=>{
+console.log(`Listening on PORT: ${PORT}`);
+});
