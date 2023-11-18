ec5a6ce67caac54ef9a78cde6ef87d4a94dd3e49
Carissa Ward <cward52@my.bcit.ca>
2023-11-18 15:05:20 -0800

    added indexRouter.js, index.ejs, footer.ejs, header.ejs, and full-width.ejs

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 
 // Import express
 const express = require("express");
+// Import EJS layouts
+const expressLayouts = require("express-ejs-layouts");
+// Import body parser
+const bodyParser = require("body-parser");
 // Import path
 const path = require("path");
 // Create a server
@@ -10,14 +14,38 @@ const app = express();
 const PORT = process.env.PORT || 3003;
 // Import morgan 
 const logger = require("morgan");
+
 // Use morgan as logger
 app.use(logger("dev"));
+// Ask app to parse any URL encoded data that is sent with requests
+app.use(bodyParser.urlencoded({ extended: true}));
 // Ask app to use public dir for static files
 app.use(express.static(path.join(__dirname,"/public")));
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+
+
 // Import api router
-const apiRouter = require("./routers/apiRouter")
+const apiRouter = require("./routers/apiRouter");
+// Import profiles router
+const profilesRouter = require("./routers/indexRouter");
+// Import index router
+const indexRouter = require("./routers/indexRouter");
+
 // Use api router for /api routes
 app.use("/api",apiRouter);
+// Use profiles router for /profiles routes
+app.use("/profiles", profilesRouter);
+// Use index router for /index routes
+app.use("/", indexRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send("Not Found");
+});
+
 // Start the server
 app.listen(PORT,()=>{
 console.log(`Listening on PORT: ${PORT}`);
--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,1 +1,40 @@
 "use strict"
+
+// Import express
+const express = require("express");
+// Initialise index router
+const indexRouter = express.Router();
+// Import package reader
+const packageReader = require('../packageReader');
+
+// Home page route
+indexRouter.get('/', (req, res) => {
+    const contributors = packageReader.getContributors();
+    // Display a simple greeting for the home page
+    res.render('index', { title: 'Home', message: 'Welcome to our website!', layout: '../views/layouts/full-width', contributors });
+});
+
+// About page route
+indexRouter.get('/about', (req, res) => {
+    const contributors = packageReader.getContributors();
+    // Display a simple greeting for the about page
+    res.render('about', { title: 'About', message: 'About Us: Not Your Average Express Lane!', layout: '../views/layouts/full-width', contributors });
+});
+
+// Contact page route (GET)
+indexRouter.get('/contact', (req, res) => {
+    const contributors = packageReader.getContributors();
+    res.render('contact', { title: 'Contact', submitted: false, layout: '../views/layouts/full-width', contributors });
+});
+
+// Contact page route (POST)
+indexRouter.post('/contact', (req, res) => {
+    const contributors = packageReader.getContributors();
+    // Handle form submission
+    const formData = req.body;
+    // Display a thank you message after form submission
+    res.render('contact', { title: 'Contact', submitted: true, message: `Thank you, ${formData.name}!`, layout: '../views/layouts/full-width', contributors });
+  });
+
+// Export indexRouter
+module.exports = indexRouter;
